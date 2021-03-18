0f72b0b97dd43daa62f4df5a23888397ae185144
sabrina <sabourodrigues@hotmail.fr>
2021-03-18 09:09:06 +0100

    challenge6 resultat

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+function writeSecretSentence(string $param1 , string $param2):string{
+   
+    return $param1. " s'incline face à " . $param2 ;
+
+}
+echo writeSecretSentence("la mouche","la guêpe");
+
+
+
